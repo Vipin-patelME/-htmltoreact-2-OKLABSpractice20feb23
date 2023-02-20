b9feda3539a9dcb6351e14e11fe88d128898c936
Vipin-patelME <t4t.forex@gmail.com>
2023-02-20 20:12:13 +0530

    commit3

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -23,7 +23,7 @@ class Header extends Component{
                             </ul>
                             </li>
                             <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" id="navbarDropdownPortfolio" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Portfolio</a>
+                            <a className="nav-link dropdown-toggle" id="navbarDropdownPortfolio" href="/aboutS" role="button" data-bs-toggle="dropdown" aria-expanded="false">Portfolio</a>
                             <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownPortfolio">
                                 <li><a className="dropdown-item" href="portfolio-overview.html">Portfolio Overview</a></li>
                                 <li><a className="dropdown-item" href="portfolio-item.html">Portfolio Item</a></li>
